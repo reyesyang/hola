d5a17ddd724493ba5942e076d0b7396543da3bf6
Reyes Yang <reyes.yang@gmail.com>
2013-05-09 15:23:05 +0800

    add doc

--- lib/hola_reyesyang.rb
+++ lib/hola_reyesyang.rb
@@ -1,4 +1,13 @@
 class Hola
+  # Say hi to the world!
+  #
+  # Example:
+  #   >> Hola.hi('spanish')
+  #   => hola mundo
+  #
+  # Arguments:
+  #   language: (String)
+
   def self.hi(language = :english)
     translator = Translator.new(language)
     translator.hi
